0a219670db1b7bfb95613f3797f9d3326dbf4e4d
austin <aletson2357@gmail.com>
2020-02-24 22:58:26 -0500

    deleted org

--- init-navigation.el
+++ init-navigation.el
@@ -1,13 +1,13 @@
 
 
+
+
 ;; Helm configuration
 (require 'helm-config)
 
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
-(define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
-
 
 (helm-mode 1)
 
--- init.el
+++ init.el
@@ -8,20 +8,5 @@
 (load "~/.emacs.d/init-packages")
 (load "~/.emacs.d/init-display")
 (load "~/.emacs.d/init-navigation")
-(load "~/.emacs.d/init-org")
 (load "~/.emacs.d/init-python")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (flycheck elpy wttrin muse magit helm dracula-theme calfw-org calfw))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
